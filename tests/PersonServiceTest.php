c01df5881ffc920cdc5bf41981905bc1b1a757f3
George Dawoud <george@dawouds.com>
2016-10-14 22:36:58 -0700

    kida working tests

--- tests/PersonServiceTest.php
+++ tests/PersonServiceTest.php
@@ -1,22 +1,34 @@
 <?php
 
+$sSERVERNAME = 'localhost';
+$sUSER = "churchcrm";
+$sPASSWORD = "churchcrm";
+$sDATABASE = "churchcrm";
+
+$sRootPath = "../src";
+
+
+require_once "../src/Include/LoadConfigs.php";
+$_SESSION = array();
+$_SESSION['sRootPath'] = $sRootPath;
+$_SESSION['iUserID']  = 1;
+
+
+require_once dirname(__FILE__) . '/../src/vendor/autoload.php';
+require_once dirname(__FILE__) . '/../src/orm/conf/config.php';
+
+use ChurchCRM\Service\PersonService;
+
 class PersonServiceTest extends PHPUnit_Framework_TestCase {
   protected $backupGlobals = FALSE;
 
   public function testSearch() {
-    $_SESSION = array();
     $_SESSION['bAdmin'] = true;
     $personService = new PersonService();
     $results = $personService->search("admin");
     $this->assertNotEmpty($results);
   }
 
-  public function testdeleteUploadedPhoto() {
-    $_SESSION = array();
-    $_SESSION['bAdmin'] = true;
-    $personService = new PersonService();
-    $deleted = $personService->deleteUploadedPhoto(1);
-    $this->assertFalse($deleted);
-  }
+
 
 }
